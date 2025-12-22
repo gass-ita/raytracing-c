27a2e6819c027495a05cea3a2ff92fe938f9e036
gass-ita <andreanassi231205@gmail.com>
2025-12-22 21:32:35 +0100

    Refactor screen dimensions to use defined constants for width and height

--- main.c
+++ main.c
@@ -6,11 +6,14 @@
 #include <time.h>
 #include "scene.h"
 
+#define WIDTH 1920
+#define HEIGHT 1080
+
 Hittable world[] = {
     {HITTABLE_SPHERE, .object.sphere = {{0, 0, -1}, 0.5}, {.color = {0.7, 0.3, 0.3}, .reflectivity = 0.1}},       // Reflective Red Sphere
     {HITTABLE_SPHERE, .object.sphere = {{1, 0, -1.5}, 0.5}, {.color = {0.3, 0.3, 0.7}, .reflectivity = 0.9}},     // Highly Reflective Blue Sphere
     {HITTABLE_SPHERE, .object.sphere = {{-1, 0, -1.5}, 0.5}, {.color = {0.3, 0.7, 0.3}, .reflectivity = 0.2}},    // Less Reflective Green Sphere
-    {HITTABLE_PLANE, .object.plane = {{0, -0.5, 0}, {0, 1, 0}}, {.color = {0.8, 0.8, 0.0}, .reflectivity = 0.1}}, // Reflective Yellow Ground Plane    ,
+    {HITTABLE_PLANE, .object.plane = {{0, -0.5, 0}, {0, 1, 0}}, {.color = {0.1, 0.2, 0.8}, .reflectivity = 0.1}}, // Reflective Yellow Ground Plane,
 };
 
 #define NUM_HITTABLES (sizeof(world) / sizeof(world[0]))
@@ -29,8 +32,8 @@ int main()
         vec3_create(-2.0, -0.725, -0.5),
         vec3_create(4.0, 0, 0),
         vec3_create(0, 2.25, 0),
-        1920,
-        1080);
+        WIDTH,
+        HEIGHT);
 
     render_scene(stdout, &scene, &camera, 50);
     return 0;
